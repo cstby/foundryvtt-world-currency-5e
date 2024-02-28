8e686b1397bfe66d2111dda570e23939957c2e6a
Kevin Mulder <kevinjohnmulder@gmail.com>
2024-02-27 21:12:36 -0500

    refactor and dnd 3.0 fix

--- scripts/convert.js
+++ scripts/convert.js
@@ -4,20 +4,20 @@
 
 /** Converts a given value in gold to its value in the game's standard currency. */
 function gpToStandard(n) {
-    let standard = game.settings.get("world-currency-5e", "Standard");
-    return n * CONFIG.DND5E.currencies[standard].conversion;
+	let standard = game.settings.get("world-currency-5e", "Standard");
+	return n * CONFIG.DND5E.currencies[standard].conversion;
 }
 
 /** Converts a given value in the game's standard currency to gold. */
 function standardToGp(n) {
-    let standard = game.settings.get("world-currency-5e", "Standard");
-    return n / CONFIG.DND5E.currencies[standard].conversion;
+	let standard = game.settings.get("world-currency-5e", "Standard");
+	return n / CONFIG.DND5E.currencies[standard].conversion;
 }
 
 /** Adds the abbreviation of the standard currency after the given value. */
 function formatCurrency(n) {
-    let standard = game.settings.get("world-currency-5e", "Standard");
-    return String(n) + " " + CONFIG.DND5E.currencies[standard].abbreviation
+	let standard = game.settings.get("world-currency-5e", "Standard");
+	return String(n) + " " + CONFIG.DND5E.currencies[standard].abbreviation;
 }
 
-export { gpToStandard, standardToGp, formatCurrency};
+export { gpToStandard, standardToGp, formatCurrency };
