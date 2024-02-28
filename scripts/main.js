cde831f3b8f9cd6836b065973c5478d7a34098df
Kevin Mulder <kevinjohnmulder@gmail.com>
2024-02-27 21:18:32 -0500

    whitespace 4 spaces

--- scripts/main.js
+++ scripts/main.js
@@ -10,80 +10,80 @@ import * as convert from "./convert.js";
 // Core Hooks
 
 Hooks.once("init", () => {
-  registerSettings();
+    registerSettings();
 });
 
 Hooks.on("ready", function () {
-  core.patchCurrencies();
+    core.patchCurrencies();
 });
 
 Hooks.on("renderActorSheet5eCharacter", (sheet, html) => {
-  core.removeCurrencies(html);
-  // This is only necessary for tidy5e.
-  compatibility.alterCharacterCurrency(html);
-  console.log("world-currency-5e | Altered character sheet");
+    core.removeCurrencies(html);
+    // This is only necessary for tidy5e.
+    compatibility.alterCharacterCurrency(html);
+    console.log("world-currency-5e | Altered character sheet");
 });
 
 Hooks.on("renderActorSheet5eVehicle", (sheet, html) => {
-  core.removeCurrencies(html);
-  console.log("world-currency-5e | Altered vehicle sheet");
+    core.removeCurrencies(html);
+    console.log("world-currency-5e | Altered vehicle sheet");
 });
 
 Hooks.on("renderItemSheet", (sheet, html, data) => {
-  let standard = game.settings.get("world-currency-5e", "Standard");
-  if (!(game.user.isGM && standard == "gp")) {
-    html.find('[name="system.price"]').prop("disabled", true);
-    html.find('[name="system.price"]').prop("type", "text");
-    html.find('[name="system.price"]').val(convert.formatCurrency(convert.gpToStandard(data.system.price)));
-  }
+    let standard = game.settings.get("world-currency-5e", "Standard");
+    if (!(game.user.isGM && standard == "gp")) {
+        html.find('[name="system.price"]').prop("disabled", true);
+        html.find('[name="system.price"]').prop("type", "text");
+        html.find('[name="system.price"]').val(convert.formatCurrency(convert.gpToStandard(data.system.price)));
+    }
 });
 
 // Compatibility with other modules:
 
 // Tidy 5e NPC Sheet
 Hooks.on("renderActorSheet5eNPC", (sheet, html) => {
-  if (game.modules.get("tidy5e-sheet")?.active && sheet.constructor.name === "Tidy5eNPC") {
-    compatibility.alterCharacterCurrency(html);
-    core.removeCurrencies(html);
-    console.log("world-currency-5e | Altered Tidy5eNPC");
-  }
+    if (game.modules.get("tidy5e-sheet")?.active && sheet.constructor.name === "Tidy5eNPC") {
+        compatibility.alterCharacterCurrency(html);
+        core.removeCurrencies(html);
+        console.log("world-currency-5e | Altered Tidy5eNPC");
+    }
 });
 
 // Tidy 5e Vehicle Sheet
 Hooks.on("renderActorSheet5eVehicle", (sheet, html) => {
-  if (game.modules.get("tidy5e-sheet")?.active && sheet.constructor.name === "Tidy5eVehicle") {
-    compatibility.alterCharacterCurrency(html);
-    console.log("world-currency-5e | Altered Tidy5eVehicle");
-  }
+    if (game.modules.get("tidy5e-sheet")?.active && sheet.constructor.name === "Tidy5eVehicle") {
+        compatibility.alterCharacterCurrency(html);
+        console.log("world-currency-5e | Altered Tidy5eVehicle");
+    }
 
-  if (game.settings.get("world-currency-5e", "RemoveConverter")) {
-    core.removeConvertCurrency(html);
-  }
+    if (game.settings.get("world-currency-5e", "RemoveConverter")) {
+        core.removeConvertCurrency(html);
+    }
 });
 
 // Let's Trade 5e
 Hooks.on("renderTradeWindow", (sheet, html) => {
-  compatibility.alterTradeWindowCurrency(html);
-  console.log("world-currency-5e | Altered Trade Window Currency");
+    compatibility.alterTradeWindowCurrency(html);
+    console.log("world-currency-5e | Altered Trade Window Currency");
 });
 
 Hooks.on("renderDialog", (sheet, html) => {
-  if (game.modules.get("world-currency-5e")?.active && sheet.title === "Incoming Trade Request") {
-    compatibility.alterTradeDialogCurrency(html);
-    console.log("world-currency-5e | Altered Trade Dialog Currency");
-  }
+    if (game.modules.get("world-currency-5e")?.active && sheet.title === "Incoming Trade Request") {
+        compatibility.alterTradeDialogCurrency(html);
+        console.log("world-currency-5e | Altered Trade Dialog Currency");
+    }
 });
 
 // Party Overview
 Hooks.on("renderPartyOverviewApp", (sheet, html) => {
-  compatibility.alterPartyOverviewWindowCurrency(html);
-  console.log("world-currency-5e | Altered Party Overview");
+    compatibility.alterPartyOverviewWindowCurrency(html);
+    console.log("world-currency-5e | Altered Party Overview");
 });
 
 // Loot Sheet 5e
 Hooks.on("renderLootSheet5eNPC", (sheet, html, data) => {
-  $.each($(".item-price"), function (index, value) {
-    $(value).text(convert.formatCurrency(convert.gpToStandard(parseInt($(value).text()))));
-  });
-  console.log("world-currency-5e | Altered Sheet");
+    $.each($(".item-price"), function (index, value) {
+        $(value).text(convert.formatCurrency(convert.gpToStandard(parseInt($(value).text()))));
+    });
+    console.log("world-currency-5e | Altered Sheet");
 });
--- scripts/world-currency-5e.js
+++ scripts/world-currency-5e.js
@@ -4,117 +4,117 @@
 
 /** Gets the currencies specified by the user and returns them as an object.*/
 function getCurrencySettings() {
-  return {
-    cpConvert: game.settings.get("world-currency-5e", "cpConvert"),
-    spConvert: game.settings.get("world-currency-5e", "spConvert"),
-    epConvert: game.settings.get("world-currency-5e", "epConvert"),
-    gpConvert: game.settings.get("world-currency-5e", "gpConvert"),
-    ppConvert: game.settings.get("world-currency-5e", "ppConvert"),
-    cpAltRemove: game.settings.get("world-currency-5e", "cpAltRemove"),
-    spAltRemove: game.settings.get("world-currency-5e", "spAltRemove"),
-    epAltRemove: game.settings.get("world-currency-5e", "epAltRemove"),
-    gpAltRemove: game.settings.get("world-currency-5e", "gpAltRemove"),
-    ppAltRemove: game.settings.get("world-currency-5e", "ppAltRemove"),
-    cpAlt: game.settings.get("world-currency-5e", "cpAlt"),
-    spAlt: game.settings.get("world-currency-5e", "spAlt"),
-    epAlt: game.settings.get("world-currency-5e", "epAlt"),
-    gpAlt: game.settings.get("world-currency-5e", "gpAlt"),
-    ppAlt: game.settings.get("world-currency-5e", "ppAlt"),
-    cpAltAbrv: game.settings.get("world-currency-5e", "cpAltAbrv"),
-    spAltAbrv: game.settings.get("world-currency-5e", "spAltAbrv"),
-    epAltAbrv: game.settings.get("world-currency-5e", "epAltAbrv"),
-    gpAltAbrv: game.settings.get("world-currency-5e", "gpAltAbrv"),
-    ppAltAbrv: game.settings.get("world-currency-5e", "ppAltAbrv"),
-  };
+    return {
+        cpConvert: game.settings.get("world-currency-5e", "cpConvert"),
+        spConvert: game.settings.get("world-currency-5e", "spConvert"),
+        epConvert: game.settings.get("world-currency-5e", "epConvert"),
+        gpConvert: game.settings.get("world-currency-5e", "gpConvert"),
+        ppConvert: game.settings.get("world-currency-5e", "ppConvert"),
+        cpAltRemove: game.settings.get("world-currency-5e", "cpAltRemove"),
+        spAltRemove: game.settings.get("world-currency-5e", "spAltRemove"),
+        epAltRemove: game.settings.get("world-currency-5e", "epAltRemove"),
+        gpAltRemove: game.settings.get("world-currency-5e", "gpAltRemove"),
+        ppAltRemove: game.settings.get("world-currency-5e", "ppAltRemove"),
+        cpAlt: game.settings.get("world-currency-5e", "cpAlt"),
+        spAlt: game.settings.get("world-currency-5e", "spAlt"),
+        epAlt: game.settings.get("world-currency-5e", "epAlt"),
+        gpAlt: game.settings.get("world-currency-5e", "gpAlt"),
+        ppAlt: game.settings.get("world-currency-5e", "ppAlt"),
+        cpAltAbrv: game.settings.get("world-currency-5e", "cpAltAbrv"),
+        spAltAbrv: game.settings.get("world-currency-5e", "spAltAbrv"),
+        epAltAbrv: game.settings.get("world-currency-5e", "epAltAbrv"),
+        gpAltAbrv: game.settings.get("world-currency-5e", "gpAltAbrv"),
+        ppAltAbrv: game.settings.get("world-currency-5e", "ppAltAbrv"),
+    };
 }
 
 /** Writes the user-provided currencies to the CONFIG.DND5E object. */
 function patchCurrencies() {
-  let currencySettings = getCurrencySettings();
+    let currencySettings = getCurrencySettings();
 
-  game.i18n.translations.DND5E.CurrencyPP = currencySettings["ppAlt"];
-  game.i18n.translations.DND5E.CurrencyGP = currencySettings["gpAlt"];
-  game.i18n.translations.DND5E.CurrencyEP = currencySettings["epAlt"];
-  game.i18n.translations.DND5E.CurrencySP = currencySettings["spAlt"];
-  game.i18n.translations.DND5E.CurrencyCP = currencySettings["cpAlt"];
+    game.i18n.translations.DND5E.CurrencyPP = currencySettings["ppAlt"];
+    game.i18n.translations.DND5E.CurrencyGP = currencySettings["gpAlt"];
+    game.i18n.translations.DND5E.CurrencyEP = currencySettings["epAlt"];
+    game.i18n.translations.DND5E.CurrencySP = currencySettings["spAlt"];
+    game.i18n.translations.DND5E.CurrencyCP = currencySettings["cpAlt"];
 
-  game.i18n.translations.DND5E.CurrencyAbbrPP = currencySettings["ppAltAbrv"];
-  game.i18n.translations.DND5E.CurrencyAbbrGP = currencySettings["gpAltAbrv"];
-  game.i18n.translations.DND5E.CurrencyAbbrEP = currencySettings["epAltAbrv"];
-  game.i18n.translations.DND5E.CurrencyAbbrSP = currencySettings["spAltAbrv"];
-  game.i18n.translations.DND5E.CurrencyAbbrCP = currencySettings["cpAltAbrv"];
+    game.i18n.translations.DND5E.CurrencyAbbrPP = currencySettings["ppAltAbrv"];
+    game.i18n.translations.DND5E.CurrencyAbbrGP = currencySettings["gpAltAbrv"];
+    game.i18n.translations.DND5E.CurrencyAbbrEP = currencySettings["epAltAbrv"];
+    game.i18n.translations.DND5E.CurrencyAbbrSP = currencySettings["spAltAbrv"];
+    game.i18n.translations.DND5E.CurrencyAbbrCP = currencySettings["cpAltAbrv"];
 
-  CONFIG.DND5E.currencies = {
-    pp: {
-      label: currencySettings["ppAlt"],
-      abbreviation: currencySettings["ppAltAbrv"],
-      conversion: currencySettings["ppConvert"],
-    },
-    gp: {
-      label: currencySettings["gpAlt"],
-      abbreviation: currencySettings["gpAltAbrv"],
-      // Added explicit rates for easier conversion later. (see gpToStandard below.)
-      conversion: currencySettings["gpConvert"],
-    },
-    ep: {
-      label: currencySettings["epAlt"],
-      abbreviation: currencySettings["epAltAbrv"],
-      conversion: currencySettings["epConvert"],
-    },
-    sp: {
-      label: currencySettings["spAlt"],
-      abbreviation: currencySettings["spAltAbrv"],
-      conversion: currencySettings["spConvert"],
-    },
-    cp: {
-      label: currencySettings["cpAlt"],
-      abbreviation: currencySettings["cpAltAbrv"],
-      conversion: currencySettings["cpConvert"],
-    },
-  };
+    CONFIG.DND5E.currencies = {
+        pp: {
+            label: currencySettings["ppAlt"],
+            abbreviation: currencySettings["ppAltAbrv"],
+            conversion: currencySettings["ppConvert"],
+        },
+        gp: {
+            label: currencySettings["gpAlt"],
+            abbreviation: currencySettings["gpAltAbrv"],
+            // Added explicit rates for easier conversion later. (see gpToStandard below.)
+            conversion: currencySettings["gpConvert"],
+        },
+        ep: {
+            label: currencySettings["epAlt"],
+            abbreviation: currencySettings["epAltAbrv"],
+            conversion: currencySettings["epConvert"],
+        },
+        sp: {
+            label: currencySettings["spAlt"],
+            abbreviation: currencySettings["spAltAbrv"],
+            conversion: currencySettings["spConvert"],
+        },
+        cp: {
+            label: currencySettings["cpAlt"],
+            abbreviation: currencySettings["cpAltAbrv"],
+            conversion: currencySettings["cpConvert"],
+        },
+    };
 
-  console.log("world-currency-5e | Patched Currencies");
+    console.log("world-currency-5e | Patched Currencies");
 }
 
 /** Removes the currency converter from the given character sheet. */
 function removeConvertCurrency(html) {
-  html.find('[class="currency-item convert"]').remove();
-  html.find('[data-action="convertCurrency"]').remove();
-  html.find('[title="Convert Currency"]').remove();
-  // D&D5e 3.0
-  html.find(`[class="currency"]`)?.find(`[class="item-action unbutton"]`)?.remove();
+    html.find('[class="currency-item convert"]').remove();
+    html.find('[data-action="convertCurrency"]').remove();
+    html.find('[title="Convert Currency"]').remove();
+    // D&D5e 3.0
+    html.find(`[class="currency"]`)?.find(`[class="item-action unbutton"]`)?.remove();
 }
 
 /** Removes specified currency from character sheet */
 function removeCurrency(html, currency) {
-  html.find(`[class="currency-item ${currency}"]`)?.remove();
-  html.find(`[class="denomination ${currency}"]`)?.remove();
-  html.find(`[name="system.currency.${currency}"]`)?.remove();
-  // tidy-sheet
-  html.find(`[class="currency-item ${currency} svelte-52d1bs"]`)?.remove();
-  // d&d5e 3.0
-  html.find(`[class="currency ${currency}"]`)?.remove();
+    html.find(`[class="currency-item ${currency}"]`)?.remove();
+    html.find(`[class="denomination ${currency}"]`)?.remove();
+    html.find(`[name="system.currency.${currency}"]`)?.remove();
+    // tidy-sheet
+    html.find(`[class="currency-item ${currency} svelte-52d1bs"]`)?.remove();
+    // d&d5e 3.0
+    html.find(`[class="currency ${currency}"]`)?.remove();
 }
 
 function removeCurrencies(html) {
-  if (game.settings.get("world-currency-5e", "RemoveConverter")) {
-    removeConvertCurrency(html);
-  }
-  if (game.settings.get("world-currency-5e", "cpAltRemove")) {
-    removeCurrency(html, "cp");
-  }
-  if (game.settings.get("world-currency-5e", "spAltRemove")) {
-    removeCurrency(html, "sp");
-  }
-  if (game.settings.get("world-currency-5e", "epAltRemove")) {
-    removeCurrency(html, "ep");
-  }
-  if (game.settings.get("world-currency-5e", "gpAltRemove")) {
-    removeCurrency(html, "gp");
-  }
-  if (game.settings.get("world-currency-5e", "ppAltRemove")) {
-    removeCurrency(html, "pp");
-  }
+    if (game.settings.get("world-currency-5e", "RemoveConverter")) {
+        removeConvertCurrency(html);
+    }
+    if (game.settings.get("world-currency-5e", "cpAltRemove")) {
+        removeCurrency(html, "cp");
+    }
+    if (game.settings.get("world-currency-5e", "spAltRemove")) {
+        removeCurrency(html, "sp");
+    }
+    if (game.settings.get("world-currency-5e", "epAltRemove")) {
+        removeCurrency(html, "ep");
+    }
+    if (game.settings.get("world-currency-5e", "gpAltRemove")) {
+        removeCurrency(html, "gp");
+    }
+    if (game.settings.get("world-currency-5e", "ppAltRemove")) {
+        removeCurrency(html, "pp");
+    }
 }
 
 export { getCurrencySettings, patchCurrencies, removeConvertCurrency, removeCurrencies };
